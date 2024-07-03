47842c3ec642ad5ec5ea0d4ad6a90fb424be4f80
John William <johnwilliams1756@gmail.com>
2024-07-03 03:42:07 +0300

    adding a provider

---
+++ app/(modals)/lock.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Page() {
+    return (
+        <View style={{flex: 1, backgroundColor: '#fff'}}>
+            <Text>Page</Text>
+        </View>
+    );
+};
---
+++ app/(modals)/white.tsx
@@ -0,0 +1,8 @@
+import { View } from 'react-native';
+
+export default function Page() {
+    return (
+        <View style={{flex: 1, backgroundColor: '#fff'}}>
+        </View>
+    )
+}
